c84caf1a90e54e0c3b532bb7f4da03a905761341
rehanuddin1991 <rehan.ictd@gmail.com>
2024-09-23 23:59:28 +0600

    about faq components

--- src/components/shared/Carousel.jsx
+++ src/components/shared/Carousel.jsx
@@ -5,12 +5,12 @@ const Carousel = () => {
   return (
     <div className='mt-6'>
         <div className="hero bg-base-200  ">
-  <div className="hero-content flex-col lg:flex-row-reverse">
+  <div className="hero-content flex-col lg:flex-row-reverse gap-5 justify-between">
     <img
       src={banner}
-      className="max-w-sm rounded-lg shadow-2xl" />
+      className=" lg:max-w-md md:max-w-md sm:max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Unlock a World of Stories- Find Your Next Favorite Book at Book's Heaven</h1>
+      <h1 className="text-5xl font-semibold text-justify">Unlock a World of Stories- Find Your Next Favorite Book at Book's Heaven</h1>
        <br />
       <button className="btn btn-primary">Buy Book</button>
     </div>
